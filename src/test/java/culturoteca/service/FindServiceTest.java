7a593d0d63224902cdb99f3417bdfa3c318df24a
Daniel Durango <daniel.durango@udea.edu.co>
2024-05-14 20:39:17 -0500

    Adding Mocking repositories in service test cases

--- src/test/java/culturoteca/service/FindServiceTest.java
+++ src/test/java/culturoteca/service/FindServiceTest.java
@@ -16,50 +16,60 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 public class FindServiceTest {
-    private CultureMediaService culturotecaService;
-    private final VideoRepository videoRepository = Mockito.mock();
-
-     Video newVideo1 = new Video("01", "Título 1", "----", 4.5);
-     Video newVideo2  = new Video("02", "Título 2", "----", 5.5);
-     Video newVideo3  = new Video("03", "Título 3", "----", 4.4);
-     Video newVideo4  = new Video("04", "Título 4", "----", 3.5);
-     Video newVideo5  = new Video("05", "Clic 5", "----", 5.7);
-     Video newVideo6  = new Video("06", "Clic 6", "----", 5.1);
+    private CultureMediaService cultureMediaService;
+    private VideoRepository videoRepository = Mockito.mock();
 
     @BeforeEach
-    void unit(){
-        ViewsRepository viewsRepository = new ViewsRepositoryImpl();
-        culturotecaService = new CultureMediaServiceImpl(videoRepository, viewsRepository);
-    }
+    void init() {
+        videoRepository = Mockito.mock(VideoRepository.class);
+        ViewsRepository viewsRepository = Mockito.mock(ViewsRepository.class);
+        cultureMediaService = new CultureMediaServiceImpl(videoRepository, viewsRepository);
 
+        List<Video> videos = List.of(
+                new Video("01", "ExampleTitle1", "Horror", 1.5),
+                new Video("02", "ExampleTitle2", "Comedy", 2.5),
+                new Video("03", "ExampleTitle3", "Romance", 6.4),
+                new Video("04", "ExampleTitle4", "Action", 1.3),
+                new Video("05", "ExampleTitle5", "Comedy", 2.7),
+                new Video("06", "ExampleTitle6", "Dramatic", 3.1));
+        Mockito.when(videoRepository.findAll()).thenReturn(videos);
+    }
 
     @Test
     void when_FindByTitle_only_videos_which_contains_the_word_in_the_title_should_be_returned_successfully() throws CulturotecaException {
-        doReturn(List.of(newVideo5, newVideo6))
+        doReturn(List.of(new Video("05", "ExampleTitle5", "Comedy", 2.7), new Video("06", "ExampleTitle6", "Dramatic", 3.1)))
                 .when(videoRepository)
                 .find("Clic");
+        List<Video> result = cultureMediaService.find("Clic");
+        assertEquals(2, result.size());
     }
 
     @Test
     void when_FindByTitle_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
-        VideoNotFoundException videoNotFoundException = assertThrows(VideoNotFoundException.class, () -> {
-            culturotecaService.find("video");
-        });
+        doReturn(List.of())
+                .when(videoRepository)
+                .find("video");
+        assertThrows(VideoNotFoundException.class, () -> cultureMediaService.find("video"));
     }
 
     @Test
     void when_FindByDuration_only_videos_between_the_range_should_be_returned_successfully() throws CulturotecaException {
-        doReturn(List.of(newVideo1, newVideo2, newVideo6))
+        doReturn(List.of(new Video("01", "ExampleTitle1", "Horror", 1.5), new Video("02", "ExampleTitle2", "Comedy", 2.5), new Video("06", "ExampleTitle6", "Dramatic", 3.1)))
                 .when(videoRepository)
-                .find(4.5,5.5);
+                .find(4.5, 5.5);
+        List<Video> result = cultureMediaService.find(4.5, 5.5);
+        assertEquals(3, result.size());
     }
 
     @Test
-    void when_FindByDuration_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully(){
-        VideoNotFoundException videoNotFoundException = assertThrows(VideoNotFoundException.class, () -> {
-            culturotecaService.find(9.5,10.5);
-        });
+    void when_FindByDuration_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() {
+        doReturn(List.of())
+                .when(videoRepository)
+                .find(9.5, 10.5);
+        assertThrows(VideoNotFoundException.class, () -> cultureMediaService.find(9.5, 10.5));
     }
 }
+
