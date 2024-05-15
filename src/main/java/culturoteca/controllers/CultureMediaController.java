b6f26892c547b1ff3e2959771565be002b0494d0
Daniel Durango <daniel.durango@udea.edu.co>
2024-05-15 13:39:28 -0500

    Adding "/videos".

--- src/main/java/culturoteca/controllers/CultureMediaController.java
+++ src/main/java/culturoteca/controllers/CultureMediaController.java
@@ -29,7 +29,7 @@ public class CultureMediaController {
         this.cultureMediaService = new CultureMediaServiceImpl(new VideoRepositoryImpl(), new ViewsRepositoryImpl());
     }
 
-    @GetMapping("/video")
+    @GetMapping("/videos")
     public ResponseEntity<List<Video>> findAllVideos() {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(cultureMediaService.findAll());
